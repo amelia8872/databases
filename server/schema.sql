f37c96fdaeb8df0890f2808863d9c8281eb3af29
Amelia LI <jkramelia8872@gmail.com>
2023-07-26 21:00:59 -0400

    complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,38 @@
+DROP DATABASE if exists chat;
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id int NOT NULL,
+  name varchar(20),
+  PRIMARY KEY (id)
+
+);
+
+CREATE TABLE rooms (
+  id int NOT NULL,
+  name varchar(20),
+  PRIMARY KEY (id)
+
+);
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id int NOT NULL,
+  user_id int,
+  room_id int,
+  text varchar(500),
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+
 );
+
+
+
+
 
 /* Create other tables and define schemas for them here! */
 
